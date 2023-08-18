8bf7aa54f516994dd14afa5b6729bbaa173dde83
Abdelhak RAHMOUNI <rahmouni.abdelhak.1@gmail.com>
2023-08-18 22:31:10 +0100

    Implement: scrap each book details

--- commands.md
+++ commands.md
@@ -3,3 +3,9 @@ Create python virtual environment
 
 Start an already created virtual environment in terminal
 `source venv/bin/activate`
+
+Run a spider using Scrapy
+`scrapy crawl bookspider`
+
+Run a spider using Scrapy and save output to a file
+`scrapy crawl bookspider -O bookdata.csv`
--- part-2/venv/bookscraper/bookscraper/spiders/bookspider.py
+++ part-2/venv/bookscraper/bookscraper/spiders/bookspider.py
@@ -2,28 +2,44 @@ import scrapy
 
 
 class BookspiderSpider(scrapy.Spider):
-    name = "bookspider"
-    allowed_domains = ["books.toscrape.com"]
-    start_urls = ["https://books.toscrape.com/"]
+	name = "bookspider"
+	allowed_domains = ["books.toscrape.com"]
+	start_urls = ["https://books.toscrape.com/"]
 
-    def parse(self, response):
-        books = response.css('article.product_pod')
-        for book in books:
-            yield {
-                'name': book.css('h3 a::text').get(),
-                'price': book.css('.product_price .price_color::text').get(),
-                'url': book.css('h3 a').attrib['href'],
-            }
-        
-        next_page = response.css('li.next a ::attr(href)').get()
-        if next_page is not None:
-            if 'catalogue' in next_page:
-                next_page_url = 'https://books.toscrape.com/' + next_page
-            else:
-                next_page_url = 'https://books.toscrape.com/catalogue/' + next_page
+	def parse(self, response):
+		books = response.css('article.product_pod')
+		for book in books:
+			relative_url = book.css('h3 a').attrib['href']
+			if 'catalogue/' in relative_url:
+				book_url = 'https://books.toscrape.com/' + relative_url
+			else:
+				book_url = 'https://books.toscrape.com/catalogue/' + relative_url
+			yield response.follow(book_url, callback = self.parse_book_page)
+		
+		next_page = response.css('li.next a').attrib['href']
+		if next_page is not None:
+			if 'catalogue/' in next_page:
+				next_page_url = 'https://books.toscrape.com/' + next_page
+			else:
+				next_page_url = 'https://books.toscrape.com/catalogue/' + next_page
+			yield response.follow(next_page_url, callback = self.parse)
+		
+	def parse_book_page(self, response):
+		table_rows = response.css('table tr')
+		yield {
+			'title': response.css('.col-sm-6.product_main h1::text').get(),
+			'category': response.css('ul.breadcrumb :nth-child(3) a ::text').get(),
+			'description': response.css('#product_description + p::text').get(),
+			'price': response.css('p.price_color::text').get(),
+			'product_type': table_rows[1].css('td ::text').get(),
+			'price_excl_tax': table_rows[2].css('td ::text').get(),
+			'price_incl_tax': table_rows[3].css('td ::text').get(),
+			'tax': table_rows[4].css('td ::text').get(),
+			'availability': table_rows[5].css('td ::text').get(),
+			'rating': response.css('p.star-rating').attrib['class'].split(' ')[1],
+			'url': response.url
+		}
 
-            yield response.follow(next_page_url, callback = self.parse)
+	
 
-    
-
-    
+	
